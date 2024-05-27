b1360f9f38c82b121bdc96ef347b189851c90e07
Duncan <167258342+DuncanBryson@users.noreply.github.com>
2024-05-27 09:57:53 -0700

    Made app to exercise 1.6

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,5 +1,11 @@
 import { useState } from 'react'
 
+const Header = ({text}) => <h1>{text}</h1>
+
+const Button = ({handleClick, text}) => (
+  <button onClick={handleClick}>{text}</button>
+)
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
@@ -8,7 +14,16 @@ const App = () => {
 
   return (
     <div>
-      code here
+      <Header text = "Give Feedback"/>
+      <Button handleClick={() => setGood(good + 1)} text="Good"/>
+      <Button handleClick={() => setNeutral(neutral + 1)} text="Neutral"/>
+      <Button handleClick={() => setBad(bad + 1)} text="Bad"/>
+      <Header text = "Statistics" />
+      <div>
+        <p>Good {good}</p>
+        <p>Neutral {neutral}</p>
+        <p>Bad {bad}</p>
+      </div>
     </div>
   )
 }
