b7887ac87d362680ed53491263744d6d425c75f9
Duncan <167258342+DuncanBryson@users.noreply.github.com>
2024-05-27 16:21:20 -0700

    added name input as 2.6

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,23 +2,31 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas'}
   ]) 
   const [newName, setNewName] = useState('')
+  const addName = (event) => {
+    event.preventDefault()
+    setPersons(persons.concat({name:newName}))
+    setNewName('')
+  }
+  const handleInput = (event) => setNewName(event.target.value)
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addName}>
         <div>
-          name: <input />
+          name: <input value={newName} onChange={handleInput} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      {persons.map((person)=>
+      <p key={person.name}>{person.name}</p>
+      )}
     </div>
   )
 }
