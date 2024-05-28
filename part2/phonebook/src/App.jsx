0001b9efa3d0c56a0d7d73d70aaa6432d3935823
Duncan <167258342+DuncanBryson@users.noreply.github.com>
2024-05-27 20:48:20 -0700

    added local server, axios get db

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,4 +1,6 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+
 const Filter = ({handleFilter, filter}) =>(
   <div>
   <label htmlFor="filter">filter shown with</label>
@@ -30,12 +32,13 @@ const Persons = ({persons, filter}) => (
 )
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ]) 
+  const [persons, setPersons] = useState([]) 
+  useEffect(() => {
+    axios.get("http://localhost:3001/persons")
+      .then(response =>
+        setPersons(response.data)
+      )
+  },[])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const addName = (event) => {
