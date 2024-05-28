337375a9a7ef5eb159998c07f9fb3535f707a4b6
Duncan <167258342+DuncanBryson@users.noreply.github.com>
2024-05-28 13:39:39 -0700

    added remove to phonebook service

--- part2/phonebook/src/services/phonebook.js
+++ part2/phonebook/src/services/phonebook.js
@@ -11,5 +11,10 @@ const create = newObject => {
   return request.then(response => response.data)
 }
 
+const remove = id => {
+  const request = axios.delete(`${baseUrl}/${id}`)
+  return request
+}
 
-export default {getAll, create}
+
+export default {getAll, create, remove}
