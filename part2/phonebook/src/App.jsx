d5c9c90de1d0503492ef55916f4c4bcd00040880
Duncan <167258342+DuncanBryson@users.noreply.github.com>
2024-05-29 09:26:54 -0700

    fully broke out Form component

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -6,36 +6,16 @@ import Persons from './components/Persons'
 
 const App = () => {
   const [persons, setPersons] = useState([]) 
+
+  
   useEffect(() => {
     phonebook
       .getAll()
       .then(initialPersons =>setPersons(initialPersons))
   },[])
-  const [newName, setNewName] = useState('')
-  const [newNumber, setNewNumber] = useState('')
-  const addName = (event) => {
-    event.preventDefault()
-    const existingNames = persons.map((person) => person.name)
-    const newPerson = {name: newName, number: newNumber}
-    if (existingNames.includes(newName) && window.confirm(
-      `${newName} is already in your phonebook. Update their number?`
-    )){
-      const id = persons.find(p => p.name === newName).id
-      phonebook
-        .update(id, newPerson)
-        .then(returnPerson => {
-          setPersons(persons.map(per => per.id !== id ? per:returnPerson ))
-        })
-    }else if (!existingNames.includes(newName)){
-      phonebook
-        .create(newPerson)
-        .then (returnObject => setPersons(persons.concat(returnObject)))
-    }
-    setNewName('')
-    setNewNumber('')
-  }
-  const handleName = (event) => setNewName(event.target.value)
-  const handleNumber = (event) => setNewNumber(event.target.value)
+
+
+  
   
   const [filter, setFilter] = useState('')
   const handleFilter = (event) => setFilter(event.target.value)
@@ -54,12 +34,10 @@ const App = () => {
       <h2>Phonebook</h2>
       <Filter handleFilter={handleFilter} filter = {filter} />
       <h2>Add New:</h2>
-      <Form addName={addName}
-        newName={newName}
-        handleName={handleName}
-        newNumber={newNumber}
-        handleNumber={handleNumber}
-      />
+      <Form 
+      persons={persons}
+      setPersons={setPersons}
+       />
       <h2>Numbers</h2>
       <Persons 
         persons={persons} 
--- part2/phonebook/src/components/Form.jsx
+++ part2/phonebook/src/components/Form.jsx
@@ -1,18 +1,47 @@
-import React from "react"
+import {useState} from "react"
+import phonebook from "../services/phonebook"
 
-const Form = ({addName, newName, handleName, newNumber, handleNumber}) => (
-  <form onSubmit={addName}>
-        <div>
-          <label htmlFor="name">Name</label>
-          <input value={newName} onChange={handleName} id='name'/>
-        </div>
-        <div>
-          <label htmlFor="number">Number</label>
-          <input value={newNumber} onChange={handleNumber} id='number' />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
-)
+const Form = ({persons, setPersons}) => {
+  const handleName = (event) => setNewName(event.target.value)
+  const handleNumber = (event) => setNewNumber(event.target.value)
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+
+  const addName = (event) => {
+    event.preventDefault()
+    const existingNames = persons.map((person) => person.name)
+    const newPerson = {name: newName, number: newNumber}
+    if (existingNames.includes(newName) && window.confirm(
+      `${newName} is already in your phonebook. Update their number?`
+    )){
+      const id = persons.find(p => p.name === newName).id
+      phonebook
+        .update(id, newPerson)
+        .then(returnPerson => {
+          setPersons(persons.map(per => per.id !== id ? per:returnPerson ))
+        })
+    }else if (!existingNames.includes(newName)){
+      phonebook
+        .create(newPerson)
+        .then (returnObject => setPersons(persons.concat(returnObject)))
+    }
+    setNewName('')
+    setNewNumber('')
+  }
+  return (
+    <form onSubmit={addName}>
+      <div>
+        <label htmlFor="name">Name</label>
+        <input value={newName} onChange={handleName} id='name'/>
+      </div>
+      <div>
+        <label htmlFor="number">Number</label>
+        <input value={newNumber} onChange={handleNumber} id='number' />
+      </div>
+      <div>
+        <button type="submit">add</button>
+      </div>
+    </form>
+  )
+}
 export default Form
