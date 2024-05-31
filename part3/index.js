f81a705aa5b37a3027f989457ee3cb0800bc9462
Duncan <167258342+DuncanBryson@users.noreply.github.com>
2024-05-31 09:17:41 -0700

    Production build on fly as 3.10

--- part3/index.js
+++ part3/index.js
@@ -4,6 +4,7 @@ const cors = require('cors')
 const app = express()
 app.use(cors())
 app.use(express.json())
+app.use(express.static('dist'))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :content', {
     skip: (req) => req.method !== "POST"
 }))
@@ -93,7 +94,7 @@ const makeID = () => {
   } else return id
 }
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
