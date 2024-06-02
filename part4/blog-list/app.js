977356607fdb6cd07022c51c839d8c6ccbd345ac
Duncan <167258342+DuncanBryson@users.noreply.github.com>
2024-06-02 13:06:49 -0700

    Refactored into modules as 4.2 + minor additions

---
+++ part4/blog-list/app.js
@@ -0,0 +1,23 @@
+const config = require("./utils/config");
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const mongoose = require("mongoose");
+const logger = require("./utils/logger");
+const blogRouter = require("./controllers/blogs");
+
+const mongoUrl = process.env.MONGODB_URI;
+mongoose
+  .connect(mongoUrl)
+  .then(() => {
+    logger.info("connected to MongoDB");
+  })
+  .catch((err) => {
+    logger.error("Error connecting to MongoDB", err.message);
+  });
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/blogs", blogRouter);
+
+module.exports = app;
