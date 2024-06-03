41a7232a824c96b055934ed6802a435aae57f010
Duncan <167258342+DuncanBryson@users.noreply.github.com>
2024-06-02 17:59:24 -0700

    added and tested favouriteblogs as 4.5

--- part4/blog-list/utils/list_helper.js
+++ part4/blog-list/utils/list_helper.js
@@ -5,7 +5,13 @@ const dummy = (blogs) => 1;
 
 const totalLikes = (blogs) => blogs.reduce((sum, blog) => sum + blog.likes, 0);
 
+const favoriteBlog = (blogs) => {
+  const sortedBlogs = blogs.slice();
+  sortedBlogs.sort((a, b) => b.likes - a.likes);
+  return sortedBlogs[0];
+};
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 };
