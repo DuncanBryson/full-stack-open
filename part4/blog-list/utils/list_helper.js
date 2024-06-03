2fd8bca4ccbafd17f206322dd0e10cbfa0913ece
Duncan <167258342+DuncanBryson@users.noreply.github.com>
2024-06-02 17:30:06 -0700

    added + tested totalLikes as 4.4

--- part4/blog-list/utils/list_helper.js
+++ part4/blog-list/utils/list_helper.js
@@ -1,5 +1,11 @@
+const { log } = require("console");
+const blog = require("../models/blog");
+
 const dummy = (blogs) => 1;
+
+const totalLikes = (blogs) => blogs.reduce((sum, blog) => sum + blog.likes, 0);
 
 module.exports = {
   dummy,
+  totalLikes,
 };
