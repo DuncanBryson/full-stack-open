bed12b1336b10593b540cf84e8e28b7c6f5cad5e
Duncan <167258342+DuncanBryson@users.noreply.github.com>
2024-06-05 21:45:54 -0700

    test for ID as 4.9. _ID already removed

--- part4/blog-list/tests/blogs_api.test.js
+++ part4/blog-list/tests/blogs_api.test.js
@@ -1,28 +1,33 @@
 const supertest = require("supertest");
-const { test, after, beforeEach } = require("node:test");
+const { test, after, beforeEach, describe } = require("node:test");
 const Blog = require("../models/blog");
 const assert = require("assert");
 const app = require("../app");
 const mongoose = require("mongoose");
 const api = supertest(app);
 const helper = require("./test_helper");
-const { before } = require("lodash");
 
 beforeEach(async () => {
   await Blog.deleteMany({});
   await Blog.insertMany(helper.initialBlogs);
 });
+describe("GET working", () => {
+  test("blogs are JSON", async () => {
+    const response = await api
+      .get("/api/blogs")
+      .expect(200)
+      .expect("Content-Type", /application\/json/);
+    assert.strictEqual(response.body.length, 2);
+  });
 
-test("blogs are JSON", async () => {
-  const response = await api
-    .get("/api/blogs")
-    .expect(200)
-    .expect("Content-Type", /application\/json/);
-  assert.strictEqual(response.body.length, 2);
-});
-
-test("GET correct number of blogs", async () => {
-  await api.get("/api/blogs");
+  test("GET correct number of blogs", async () => {
+    await api.get("/api/blogs");
+  });
+  test("returning id not _id", async () => {
+    const blogs = await helper.blogsInDb();
+    const ids = blogs.map((b) => b.id.length);
+    assert.deepStrictEqual(ids, [24, 24]);
+  });
 });
 
 after(async () => {
