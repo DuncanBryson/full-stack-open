c9c8503fda79e55842505d15d68a581d766d1470
Duncan <167258342+DuncanBryson@users.noreply.github.com>
2024-06-10 10:54:27 -0700

    Blog form hidden with button as 5.5

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -5,6 +5,7 @@ import LoginForm from "./components/LoginForm";
 import Notification from "./components/Notification";
 import blogService from "./services/blogs";
 import loginService from "./services/login";
+import Togglable from "./components/Togglable";
 
 const App = () => {
   const [blogs, setBlogs] = useState([]);
@@ -74,7 +75,11 @@ const App = () => {
           <span>{user.username} logged in </span>
           <button onClick={handleLogout}>Logout</button>
           <h2>blogs</h2>
-          <BlogForm {...{ showNotification, setBlogs, blogs }} />
+
+          <Togglable buttonLabel="New Blog">
+            <BlogForm {...{ showNotification, setBlogs, blogs }} />
+          </Togglable>
+
           <p></p>
           {blogs.map((blog) => (
             <Blog key={blog.id} blog={blog} />
