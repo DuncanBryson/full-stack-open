cd4a2e7171031b54ba2504c0eaf4403e16fd9801
Duncan <167258342+DuncanBryson@users.noreply.github.com>
2024-06-12 16:00:29 -0700

    Creating blogs tested as 5.19

--- part5/bloglist-frontend/playwright/tests/blogapp.spec.js
+++ part5/bloglist-frontend/playwright/tests/blogapp.spec.js
@@ -32,4 +32,23 @@ describe("Blog app", () => {
       await expect(notification).toHaveCSS("color", "rgb(255, 0, 0)");
     });
   });
+  describe("When logged in", () => {
+    beforeEach(async ({ page }) => {
+      await login(page, "test", "sekurity");
+    });
+
+    test("a new blog can be created", async ({ page }) => {
+      await page.getByRole("button", { name: "New Blog" }).click();
+      await page.getByLabel("Title").fill("New Blog Title");
+      await page.getByLabel("Author").fill("BlogAuthor");
+      await page.getByLabel("URL").fill("https://www.notawebsite.com");
+
+      await page.getByRole("button", { name: "Add Blog" }).click();
+
+      await expect(page.getByText("New Blog Title BlogAuthor")).toBeVisible();
+      const notification = await page.locator(".notification");
+      await expect(notification).toContainText("New Blog Title by BlogAuthor");
+      await expect(notification).toHaveCSS("color", "rgb(0, 128, 0)");
+    });
+  });
 });
