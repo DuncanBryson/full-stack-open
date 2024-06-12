6e66586074b376dc694ace816664a1b64dcd7111
Duncan <167258342+DuncanBryson@users.noreply.github.com>
2024-06-12 15:51:57 -0700

    E2E tested login as 5.18

--- part5/bloglist-frontend/playwright/tests/blogapp.spec.js
+++ part5/bloglist-frontend/playwright/tests/blogapp.spec.js
@@ -1,13 +1,35 @@
 const { test, expect, beforeEach, describe } = require("@playwright/test");
+const { login } = require("./helper");
 
 describe("Blog app", () => {
-  beforeEach(async ({ page }) => {
+  beforeEach(async ({ page, request }) => {
+    await request.post("/api/testing/reset");
+    await request.post("/api/users", {
+      data: {
+        name: "Tester",
+        username: "test",
+        password: "sekurity",
+      },
+    });
     await page.goto("http://localhost:5173");
-    // await request.post("/api/testing/reset");
   });
 
   test("Login form is shown", async ({ page }) => {
     await expect(page.getByText("Username")).toBeVisible();
     await expect(page.getByText("Password")).toBeVisible();
+  });
+  describe("Login", () => {
+    test("succeeds with correct credentials", async ({ page }) => {
+      await login(page, "test", "sekurity");
+      const notification = await page.locator(".notification");
+      await expect(notification).toContainText("Logged in as test");
+      await expect(notification).toHaveCSS("color", "rgb(0, 128, 0)");
+    });
+    test("fails with wrong credentials", async ({ page }) => {
+      await login(page, "test", "wrongpassword");
+      const notification = await page.locator(".notification");
+      await expect(notification).toContainText("invalid username or password");
+      await expect(notification).toHaveCSS("color", "rgb(255, 0, 0)");
+    });
   });
 });
