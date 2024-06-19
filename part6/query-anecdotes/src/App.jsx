98fed06dcfff2bbedeb3cd5ac512ce1b6f4d235e
Duncan <167258342+DuncanBryson@users.noreply.github.com>
2024-06-19 09:28:38 -0700

    Fetch anecdotes, show server errors as 6.20

--- part6/query-anecdotes/src/App.jsx
+++ part6/query-anecdotes/src/App.jsx
@@ -1,19 +1,30 @@
 import AnecdoteForm from "./components/AnecdoteForm";
 import Notification from "./components/Notification";
+import { getAnecdotes } from "./requests";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 
 const App = () => {
+  const queryClient = useQueryClient();
   const handleVote = (anecdote) => {
     console.log("vote");
   };
 
-  const anecdotes = [
-    {
-      content: "If it hurts, do it more often",
-      id: "47145",
-      votes: 0,
-    },
-  ];
+  const fetchAnecdotes = useQuery({
+    queryKey: ["anecdotes"],
+    queryFn: getAnecdotes,
+    refetchOnWindowFocus: false,
+    retry: 1,
+  });
 
+  console.log(JSON.parse(JSON.stringify(fetchAnecdotes)));
+  if (fetchAnecdotes.isLoading) return <div>Anecdotes Loading...</div>;
+  if (fetchAnecdotes.isError)
+    return (
+      console.log("Error: ", fetchAnecdotes.error.message),
+      (<div>Anecdote service not available due to server problems</div>)
+    );
+
+  const anecdotes = fetchAnecdotes.data;
   return (
     <div>
       <h3>Anecdote app</h3>
---
+++ part6/query-anecdotes/src/requests.js
@@ -0,0 +1,7 @@
+import axios from "axios";
+const baseUrl = "http://0.0.0.0:3001/anecdotes";
+
+export const getAnecdotes = async () => {
+  const result = await axios.get(baseUrl);
+  return result.data;
+};
