e30474cccc229aeb3318ac32a98db5da940265a9
DuncanBryson <167258342+DuncanBryson@users.noreply.github.com>
2024-07-05 15:47:35 -0700

    sort books by genre as 8.19

--- part8/library-frontend/src/components/Books.jsx
+++ part8/library-frontend/src/components/Books.jsx
@@ -1,8 +1,19 @@
+import { useState } from "react";
 const Books = ({ books }) => {
+  const [selectedGenre, setSelectedGenre] = useState(false);
+  const allGenres = [];
+  books.map((b) =>
+    b.genres.map((g) => {
+      allGenres.includes(g) ? null : allGenres.push(g);
+    })
+  );
+  const hidden = selectedGenre ? {} : { display: "none" };
   return (
     <div>
       <h2>books</h2>
-
+      <p style={hidden}>
+        In genre <em>{selectedGenre}</em>
+      </p>
       <table>
         <tbody>
           <tr>
@@ -10,15 +21,24 @@ const Books = ({ books }) => {
             <th>author</th>
             <th>published</th>
           </tr>
-          {books.map((a) => (
-            <tr key={a.title}>
-              <td>{a.title}</td>
-              <td>{a.author.name}</td>
-              <td>{a.published}</td>
-            </tr>
-          ))}
+          {books.map((b) => {
+            if (selectedGenre && !b.genres.includes(selectedGenre)) return null;
+            return (
+              <tr key={b.title}>
+                <td>{b.title}</td>
+                <td>{b.author.name}</td>
+                <td>{b.published}</td>
+              </tr>
+            );
+          })}
         </tbody>
       </table>
+      {allGenres.map((g) => (
+        <button key={g} onClick={() => setSelectedGenre(g)}>
+          {g}
+        </button>
+      ))}
+      <button onClick={() => setSelectedGenre(false)}>All Genres</button>
     </div>
   );
 };
